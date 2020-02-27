ce00c53daa668f02419b28216a22467f5b6e3352
Alexander Gornostaev <aleksgornostaev@gmail.com>
2020-02-27 07:29:47 +0300

    add dependency for coroutines, interface for requests and responses of protoscreens

--- calculation_sdk/src/main/java/com/reducetechnologies/calculation_sdk/CalculationSdk.kt
+++ calculation_sdk/src/main/java/com/reducetechnologies/calculation_sdk/CalculationSdk.kt
@@ -1,4 +1,9 @@
 package com.reducetechnologies.calculation_sdk
 
+/**
+ * Суть этого интерфейса - предоставлять АПИ бэка фронту, чтобы последний мог запустить вычисления
+ * и получить на них результат
+ */
 interface CalculationSdk {
+
 }
