5d377f3d3071011d63cc7b79d519a5e9ccafb222
Alexander Gornostaev <aleksgornostaev@gmail.com>
2020-04-09 19:47:09 +0300

    branch-17: updated kotlin and plugin, fixed dagger via idea plugin source settings

--- calculation_sdk/src/main/java/com/reducetechnologies/di/TestComponent.kt
+++ calculation_sdk/src/main/java/com/reducetechnologies/di/TestComponent.kt
@@ -21,3 +21,31 @@ class SomeModule() {
 interface TestComponent {
     fun produceSome() : SomeA
 }
+
+@Component(modules = [SomeModule::class])
+@Singleton
+interface TestComponent2 {
+    fun produceSome() : SomeA
+}
+
+@Component(modules = [SomeModule::class])
+@Singleton
+interface TestComponent3 {
+    fun produceSome() : SomeA
+}
+
+@Component(modules = [SomeModule::class])
+@Singleton
+interface TestComponent4 {
+    fun produceSome() : SomeA
+}
+
+fun main(string : Array<String>) {
+    val `I FUCKING HATE THIS FUCKING STUPID SHITTY DUMB SITUATION GODDAMIT` = DaggerTestComponent.builder()
+        .someModule(SomeModule())
+        .build()
+
+    val `I FUCKING LOST FUCKING DAMN TIME STUPD SHITTY DUMB AAAAAAAAAAAA` = DaggerTestComponent2.builder()
+        .someModule(SomeModule())
+        .build()
+}
