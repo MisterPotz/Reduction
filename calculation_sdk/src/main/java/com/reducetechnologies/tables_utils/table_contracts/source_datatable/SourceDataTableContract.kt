3a36cd9b387afc69087d92acbb398d0e38df458f
Dmitry Trofimov <dmitry.trofimow2011@gmail.com>
2020-05-12 17:12:44 +0300

    One thing

--- calculation_sdk/src/main/java/com/reducetechnologies/tables_utils/table_contracts/source_datatable/SourceDataTableContract.kt
+++ calculation_sdk/src/main/java/com/reducetechnologies/tables_utils/table_contracts/source_datatable/SourceDataTableContract.kt
@@ -3,8 +3,9 @@ package com.reducetechnologies.tables_utils.table_contracts.source_datatable
 import com.google.gson.*
 import com.reducetechnologies.tables_utils.TableExtractor
 import com.reduction_technologies.database.json_utils.GsonRegister
-import com.reduction_technologies.database.tables_utils.table_contracts.source_datatable.*
-import com.reduction_technologies.database.tables_utils.table_contracts.source_datatable.NWRRowGsonManager
+//Нз что тут с импортом
+/*import com.reduction_technologies.database.tables_utils.table_contracts.source_datatable.**/
+/*import com.reduction_technologies.database.tables_utils.table_contracts.source_datatable.NWRRowGsonManager*/
 
 // TODO обложить тестами на равность и сериализацию
 data class SourceDataTable(
